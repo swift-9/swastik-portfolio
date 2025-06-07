9573a167b286b9b4aedd1c64261dec124bd0fa2f
Swastik Poojary <swastikjayaraj500@gmail.com>
2025-06-07 23:16:12 +0530

    Fix: correct vite config and base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/swastik-portfolio",
+  base:"/swastik-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
